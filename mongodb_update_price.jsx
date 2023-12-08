a45bb0c09d9f7ef5c6fb014a56f76b15c630c0f7
Akanksha Giliyal <108074183+AkankshaGiliyal@users.noreply.github.com>
2023-12-08 16:44:04 +0530

    Update mongodb_update_price.jsx

--- mongodb_update_price.jsx
+++ mongodb_update_price.jsx
@@ -48,13 +48,13 @@ let fetch;
   const apiUrl1 = 'https://api.geckoterminal.com/api/v2/networks/mantle/tokens/multi/0x201eba5cc46d216ce6dc03f6a759e8e766e956ae%2C0x09bc4e0d864854c6afb6eb9a9cdf58ac190d0df9%2C0xdeaddeaddeaddeaddeaddeaddeaddeaddead1111';
   const apiUrl2 = 'https://api.geckoterminal.com/api/v2/networks/manta-pacific/tokens/multi/0xf417f5a458ec102b90352f697d6e2ac3a3d2851f%2C0xb73603c5d87fa094b7314c74ace2e64d165016fb%2C0x0dc808adce2099a9f62aa87d9670745aba741746';
 
-  // Run every 10 minutes
+  
   setInterval(() => {
     fetchDataAndUpdate(apiUrl1);
     fetchDataAndUpdate(apiUrl2);
   }, 60000);
 
-  // Run immediately
+  
   fetchDataAndUpdate(apiUrl1);
   fetchDataAndUpdate(apiUrl2);
 })();
