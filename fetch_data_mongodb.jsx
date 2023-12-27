c73e280e7e3e7eebacf3505a5f9f913af9d800c4
Akanksha Giliyal <108074183+AkankshaGiliyal@users.noreply.github.com>
2023-12-27 11:26:33 +0530

    Update fetch_data_mongodb.jsx

--- fetch_data_mongodb.jsx
+++ fetch_data_mongodb.jsx
@@ -17,45 +17,7 @@ const options = {
 // mongoDB connection URI
 const uri = 'mongodb+srv://liltest:BI6H3uJRxYOsEsYr@cluster0.qtfou20.mongodb.net/';
 
-app.get('/mantle', async (req, res) => {
-  try {
-    const client = new MongoClient(uri, { useNewUrlParser: true });
-    await client.connect();
 
-    const db = client.db('vaults');
-    const collection = db.collection('mantle');
-
-    // fetching the data from MongoDB
-    const data = await collection.find({}).toArray();
-
-    client.close();
-
-    res.json(data);
-  } catch (error) {
-    console.error('Error fetching data from MongoDB:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
-});
-
-app.get('/manta', async (req, res) => {
-  try {
-    const client = new MongoClient(uri, { useNewUrlParser: true });
-    await client.connect();
-
-    const db = client.db('vaults');
-    const collection = db.collection('manta-pacific');
-
-    // fetching the data from the second collection
-    const data = await collection.find({}).toArray();
-
-    client.close();
-
-    res.json(data);
-  } catch (error) {
-    console.error('Error fetching data from the second collection:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
-});
 app.get('/oracle/coingecko', async (req, res) => {
   try {
     const client = new MongoClient(uri, { useUnifiedTopology: true });
@@ -80,51 +42,30 @@ app.get('/tvl_usd_sum', async (req, res) => {
     await client.connect();
 
     const database = client.db('vaults');
-    const collection1 = database.collection('mantle');
-    const collection2 = database.collection('manta-pacific');
+    const collection = database.collection('vault'); 
 
-    const pipeline1 = [
+    const pipeline = [
       {
         $group: {
           _id: null,
-          totalCollection1: { $sum: '$tvlUSD' },
+          totalTvlUSD: { $sum: '$tvlUSD' },
         },
       },
       {
         $project: {
           _id: 0,
-          totalCollection1: 1,
+          totalTvlUSD: 1,
         },
       },
     ];
 
-    const pipeline2 = [
-      {
-        $group: {
-          _id: null,
-          totalCollection2: { $sum: '$tvlUSD' }, 
-        },
-      },
-      {
-        $project: {
-          _id: 0,
-          totalCollection2: 1,
-        },
-      },
-    ];
-
-    const [result1, result2] = await Promise.all([
-      collection1.aggregate(pipeline1).toArray(),
-      collection2.aggregate(pipeline2).toArray(),
-    ]);
+    const result = await collection.aggregate(pipeline).toArray();
 
     client.close();
 
-    const sum1 = result1[0]?.totalCollection1 || 0;
-    const sum2 = result2[0]?.totalCollection2 || 0;
-    const combinedSum = sum1 + sum2;
+    const totalSum = result[0]?.totalTvlUSD || 0;
 
-    res.json({ sum: combinedSum });
+    res.json({ sum: totalSum });
   } catch (error) {
     console.error('Error calculating combined TVL sum:', error);
     res.status(500).json({ error: 'Internal Server Error' });
@@ -139,29 +80,24 @@ app.get('/vaults', async (req, res) => {
     await client.connect();
 
     const database = client.db('vaults');
-
-    let collection1 = database.collection('mantle');
-    let collection2 = database.collection('manta-pacific');
+    const collection = database.collection('vault'); // Use the 'test' collection
 
     const projection = { _id: 0 };
+    let data = [];
 
-    let data1 = [];
-    let data2 = [];
+    if (chainName) {
+     
+      const filter = { chain: chainName }; 
 
-    if (chainName === 'mantle') {
-      collection1 = database.collection('mantle');
-      data1 = await collection1.find({}, projection).toArray();
-    } else if (chainName === 'manta-pacific') {
-      collection2 = database.collection('manta-pacific');
-      data2 = await collection2.find({}, projection).toArray();
+      data = await collection.find(filter, projection).toArray();
     } else {
-      data1 = await collection1.find({}, projection).toArray();
-      data2 = await collection2.find({}, projection).toArray();
+      
+      data = await collection.find({}, projection).toArray();
     }
 
     client.close();
 
-    res.json([...data1, ...data2]);
+    res.json(data);
   } catch (error) {
     console.error('Error fetching TVL data:', error);
     res.status(500).json({ error: 'Internal Server Error' });
@@ -178,19 +114,16 @@ app.get('/vaults/dex', async (req, res) => {
 
   try {
     const client = await MongoClient.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-    const db = client.db('vaults'); 
-
-    const collection1 = db.collection('mantle');
-    const collection2 = db.collection('manta-pacific');
+    const db = client.db('vaults');
+    const collection = db.collection('vault'); // Use the 'test' collection
 
     const projection = { _id: 0 };
 
-    const data1 = await collection1.find({ dex: dexValue }, projection).toArray();
-    const data2 = await collection2.find({ dex: dexValue }, projection).toArray();
+    const data = await collection.find({ dex: dexValue }, projection).toArray();
 
     client.close();
 
-    res.json([...data1, ...data2]);
+    res.json(data);
   } catch (error) {
     console.error('Error fetching data:', error);
     res.status(500).json({ error: 'Internal Server Error' });
@@ -282,24 +215,20 @@ app.get('/quant', async (req, res) => {
     await client.connect();
 
     const db = client.db('vaults');
-    const collection1 = db.collection('mantle');
-    const collection2 = db.collection('manta-pacific');
+    const collection = db.collection('vault'); 
 
-    const { value } = req.query; 
+    const { value } = req.query;
 
     let filter = {};
-    if (value === 'true') {
-      filter = { quant: true };
-    } else if (value === 'false') {
-      filter = { quant: false };
+    if (value === 'true' || value === 'false') {
+      filter = { quant: value }; // Filter based on string representations of boolean values
     }
 
-    const data1 = await collection1.find(filter).toArray();
-    const data2 = await collection2.find(filter).toArray();
+    const data = await collection.find(filter).toArray();
 
     client.close();
 
-    res.json([...data1, ...data2]);
+    res.json(data);
   } catch (error) {
     console.error('Error handling /quant route:', error);
     res.status(500).json({ error: 'Internal Server Error' });
